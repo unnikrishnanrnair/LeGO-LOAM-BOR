b68dc74e5e997ce31f19404d924d1ad301196e3b
Nishantgoyal918 <nishantgoyal918@gmail.com>
2020-12-07 02:08:53 +0530

    added comments in poseInitialization

--- LeGO-LOAM/src/poseInitialization.h
+++ LeGO-LOAM/src/poseInitialization.h
@@ -4,34 +4,55 @@
 #include "lego_loam/utility.h"
 #include <ros/package.h>
 
+/**
+ * PoseInitialization class publishes the initial position of vehicle
+ */
 class PoseInitialization {
 
 public:
+	/**
+	 * Default Constructor
+	 */
 	PoseInitialization( ros::NodeHandle& node);
 
 	~PoseInitialization();
 
+	/**
+	 * Main Function
+	 */
 	void runPoseInitialization();
+
 private:
 	ros::NodeHandle& nh;
 
-	ros::Publisher pubInitialPose;
+	ros::Publisher pubInitialPose;	// vehcile initial position publisher
 
 	std::thread _run_thread;
 
-	double init_pose_x;
-	double init_pose_y;
-	double init_pose_z;
-	double init_pose_roll;
-	double init_pose_pitch;
-	double init_pose_yaw;
+	double init_pose_x;		// vehicle initial position x coordinate
+	double init_pose_y;		// vehicle initial position y coordinate
+	double init_pose_z;		// vehicle initial position z coordinate
+	double init_pose_roll;	// vehicle initial roll
+	double init_pose_pitch;	// vehicle initial pitch
+	double init_pose_yaw;	// vehicle initial yaw
 
-	std::string init_pose_file;
+	std::string init_pose_file;	// location of inital position file
 
-	nav_msgs::Odometry intiPose;
+	nav_msgs::Odometry initPose; // initial position as odometry message
 
 private:
+	/**
+	 * Read the initial position of vehicle from init_pose_file
+	 * 
+	 * FILE FORMAT:
+	 * x y z roll pitch yaw (6 space separated values in this order)
+	 *
+	 */
 	void readPoseFromFile();
+
+	/**
+	 * publishes the vehicle's initial position as odometry message on '/tf/odometry'
+	 */
 	void publishInitialPose();
 
 };
