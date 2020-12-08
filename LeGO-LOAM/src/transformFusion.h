d0e128fe5fa054a89173738d929a92b02df524fa
Shrinivas Khiste <shrinivaskhistesk@gmail.com>
2020-12-08 23:43:04 +0530

    Added comments to TransformFusion

--- LeGO-LOAM/src/transformFusion.h
+++ LeGO-LOAM/src/transformFusion.h
@@ -6,9 +6,11 @@
 class TransformFusion {
  private:
   ros::NodeHandle& nh;
-  ros::Publisher pubLaserOdometry2;
-  ros::Subscriber subLaserOdometry;
-  ros::Subscriber subOdomAftMapped;
+  ros::Publisher pubLaserOdometry2; // Publish Odometry to /integrated_to_init
+  ros::Subscriber subLaserOdometry;  // Subscribe to laser Odometery 
+                                      // (/laser_odom_to_init)
+  ros::Subscriber subOdomAftMapped;  // Subscribe to Mapped Odometry 
+                                     // /aft_mapped_to_init  
 
   nav_msgs::Odometry laserOdometry2;
   tf::StampedTransform laserOdometryTrans2;
@@ -20,19 +22,30 @@ class TransformFusion {
   tf::StampedTransform camera_2_base_link_Trans;
   tf::TransformBroadcaster tfBroadcasterCamera2Baselink;
 
-  float transformSum[6];
+  float transformSum[6];   // The laser odometry data
   float transformIncre[6];
-  float transformMapped[6];
-  float transformBefMapped[6];
-  float transformAftMapped[6];
+  float transformMapped[6];  // The Transform Associated with map
+  float transformBefMapped[6];  
+  float transformAftMapped[6];  // The Mapped Odometry
 
   std_msgs::Header currentHeader;
 
  public:
   TransformFusion(ros::NodeHandle& node);
-
+  /**
+   * Calculate TransformMapped wrt map using 
+   * TransformBefMapped, TransformSum and TransformAftMapped
+  */
   void transformAssociateToMap();
+  /**
+   * Handler for Laser Odometry that publishes TransformMapped and 
+   * also the tf between /camera_init and /camera
+  */
   void laserOdometryHandler(const nav_msgs::Odometry::ConstPtr& laserOdometry);
+  /**
+   * Handler for Mapped Odometry that updates values of 
+   * TransformAftMapped and TransformBefMapped 
+  */
   void odomAftMappedHandler(const nav_msgs::Odometry::ConstPtr& odomAftMapped);
 };
 
